7abffd052c3c178b04e313aba788b7de7f75dc93
Pedro Yuri <pedro.yuri.rodrigues08@aluno.ifce.edu.br>
2024-02-20 14:21:57 -0300

    finalização do módulo de JavaScript Moderno

--- Fundamentos_de_JavaScript/09-hof.js
+++ Fundamentos_de_JavaScript/09-hof.js
@@ -4,7 +4,7 @@ const array = [
     {name: 'Magic Mouse', price: 1000, quantity: 5},
 ];
 
-// //.find & findIndex
+//.find & findIndex
 
 const find = array.find((product) => product.price > 1000);
 console.log({find});
@@ -16,7 +16,7 @@ console.log({findIndex});
 
 console.log('--------------------------------------------------------');
 
-// //.some & every
+//.some & every
 
 const some = array.some((product) => product.price < 1000);
 console.log({some});
@@ -28,7 +28,7 @@ console.log({every});
 
 console.log('--------------------------------------------------------');
 
-// //.map 
+//.map 
 
 const map = array.map((product) => ({
     ...product,
@@ -42,3 +42,13 @@ console.log('--------------------------------------------------------');
 
 const filter = array.filter((product) => product.quantity > 1);
 console.log({filter});
+
+// .reduce
+
+const reduce = array.reduce((accumulator, product) => {
+    return accumulator + (product.price * product.quantity);
+}, 0); // esse 0 é o valor inicial do reduce
+
+//o accumulator serve como uma variável de armazenamento
+
+console.log({ reduce });
